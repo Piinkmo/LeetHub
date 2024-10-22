0dbf1c3ccd29bdfb46bbc060cc1c36774ddaca03
Yuting Jia <112676056+Piinkmo@users.noreply.github.com>
2024-10-22 13:51:33 -0500

    Time: 413 ms (10.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+
+with CombinedFriends AS (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+),
+
+FindMost AS (
+    SELECT id, COUNT(*) as num
+    FROM CombinedFriends
+    GROUP BY id
+)
+
+SELECT id, num
+FROM FindMost
+ORDER BY num DESC
+limit 1
